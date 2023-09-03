04f5dc398cce0f489e1b44c50b0538d3dcf03119
Peder Goodman <peder.goodman@gmail.com>
2023-09-03 16:10:46 -0500

    dropbox/folder

--- server/routes/dropbox.api.js
+++ server/routes/dropbox.api.js
@@ -32,12 +32,38 @@ router.post('/folder/:userId', async (req, res) => {
     const folderId = newVendorFolder.result.metadata.id
 
 
+    // *** create new shared dropbox link based on created folder path
+    // check if a shared link exists
+    const checkForSharedLink = await dbx.sharingListSharedLinks({
+      path: folderPath
+    });
+
+    // * check if a shared link exists
+    if (checkForSharedLink.result.links[0]) {
+      // if a share link exists, set the link to a variable
+      sharedFolderLink = checkForSharedLink.result.links[0].url
+
+    } else {
+      // if a shared link does not exist, create new shared link from provided path
+      const createNewSharedLink =
+        await dbx.sharingCreateSharedLinkWithSettings({
+          path: folderPath
+        });
+
+      // set shared link to shared link
+      sharedFolderLink = createNewSharedLink.result.url
+    }
+
+
+
+    
     
 
 
 
     res.sendStatus(200)
     // ! do not need to send result, this is just for testing the response
+    console.log('userId is:', userId);
     console.log('folderPath is:', folderPath);
     console.log('folderId is:', folderId);
     console.log('sharedFolderLink is:', sharedFolderLink);
