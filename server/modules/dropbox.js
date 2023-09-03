3f7162042aeb8723542aa5638c8a00fde089ff58
Peder Goodman <peder.goodman@gmail.com>
2023-09-03 13:41:23 -0500

    bring dropbox in to server & setup router

---
+++ server/modules/dropbox.js
@@ -0,0 +1,26 @@
+const { Dropbox } = require('dropbox');
+require('dotenv').config();
+
+
+// from sandbox
+const dbx = new Dropbox({
+    accessToken: "sl.BlX3oXRdGZs0-nAi7NnoByfFL3ghVPMBw5FNZ5GEkTUtA9M9dNakTs01AnH46xKmUIbqazVHUApWVTsc-31ku41Daf1iLbBQA26FUohvBUaAv-ls8hc1__BYkuxgNhyMYnT4pJHyqKzLfac"
+}); // Replace with your access token
+
+
+
+
+
+// // create session ref:
+// const dropbox = Dropbox.authenticate({
+//     token: 'sl.Bk9fG-KcF1DguvGqKcoIcMRG4l7OcS3Yw2-DswBqLPIoTwsLB-S1Zcplr04mZ_EYeAfAF9_Fz8Ykd2wR1l1LgxOYe45KcT44vJrj46FgKK_cRbu8Wlhx6FXQbnU10431lwMXy1JeTGmL'
+// });
+
+// const APP_KEY = "yi1sy5me2a49729"; // Replace with your Dropbox app key
+// const DROPBOX_SECRET = "v33nvhfsgjwrs9q";
+// const CLIENT_ID = "dbid:AAA-iF2A0GBCEwestcaG5Q4vS5EfTSdUYVs";
+// const redirect_uri = 'http://localhost:3000/auth'
+
+
+
+module.exports = dbx;
