01c1622eb3acac303b17bd46f39bf4c47d327960
Peder Goodman <peder.goodman@gmail.com>
2023-09-09 21:44:31 -0500

    changed admin dropbox fold

--- src/components/AdminUser/VendorDetails/VendorDetails.jsx
+++ src/components/AdminUser/VendorDetails/VendorDetails.jsx
@@ -317,7 +317,7 @@ function VendorDetails({ open, onClose, vendorId }) {
                   bgcolor: "#f3aaa5",
                   boxShadow: "0px 4px 1px -2px rgba(0,0,0,0.2)", 
                   "&:hover": {
-                    backgroundColor: "green",
+                    backgroundColor: "#ef796d",
                   },
                 }}
               >
